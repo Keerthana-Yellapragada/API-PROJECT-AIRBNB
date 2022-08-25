85e955a2233b6df7ead81fe65acf8c59123e987f
Keerthana Yellapragada <keerthanay@gmail.com>
2022-08-25 16:07:19 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,39 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+router.use(restoreUser);
+
+
+module.exports = router;
+
+// ...
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
 
 
-// backend/routes/api/index.js
-// ...
+
 
 router.post('/test', function (req, res) {
     res.json({
@@ -13,4 +41,24 @@ router.post('/test', function (req, res) {
     });
 });
 
-// ...
+
+
+
+// GET /api/set-token-cookie
+const {
+    setTokenCookie
+} = require('../../utils/auth.js');
+const {
+    User
+} = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({
+        user
+    });
+});
