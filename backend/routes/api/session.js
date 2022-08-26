2abec484a77bb5046a95ba949f32f4cd50d93c18
Keerthana Yellapragada <keerthanay@gmail.com>
2022-08-26 10:57:21 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -38,6 +38,19 @@ router.post(
     }
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({
+            message: 'success'
+        });
+    }
+);
+
+
+// ...
 
 
 module.exports = router;
