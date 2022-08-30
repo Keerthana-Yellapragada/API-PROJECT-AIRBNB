d159bc39efdad226e52092a012230def916ed2a2
Keerthana Yellapragada <keerthanay@gmail.com>
2022-08-30 07:44:52 -0700

    added booking seed data

--- backend/db/seeders/20220829231322-spot-data.js
+++ backend/db/seeders/20220829231322-spot-data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,34 +10,33 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-   return queryInterface.bulkInsert('Spots', [
-    {
-      ownerId: 1,
-      address: " 1234 De Anza Ave",
-      city: "San Jose",
-      state: "CA",
-      country: "USA",
-      lat: 100.800,
-      lng: 200.400,
-      name: "Cozy Getaway in Santa Cruz",
-      description: "A fully furnished cabin nestled in the Santa Cruz hills. All amenities provided. Hot Tub included. Minimum 2 night booking. Free cancellations. No smoking. Pets allowed.",
-      price: 100.00
-    },
-      {
-          ownerId: 2,
-          address: " 5678 Domingo Drive",
-          city: "San Diego",
-          state: "CA",
-          country: "USA",
-          lat: 200.800,
-          lng: 600.400,
-          name: "Modern Beachfront Home",
-          description: "A fully furnished modern home by the beachfront. All amenities provided. Hot Tub and private pool access included. Minimum 2 night booking. No cancellations allowed. No smoking or pets allowed",
-          price: 300.00
+     */
+    return queryInterface.bulkInsert('Spots', [{
+        ownerId: 1,
+        address: " 1234 De Anza Ave",
+        city: "San Jose",
+        state: "CA",
+        country: "USA",
+        lat: 100.800,
+        lng: 200.400,
+        name: "Cozy Getaway in Santa Cruz",
+        description: "A fully furnished cabin nestled in the Santa Cruz hills. All amenities provided. Hot Tub included. Minimum 2 night booking. Free cancellations. No smoking. Pets allowed.",
+        price: 100.00
       },
-    {
-      ownerId: 1,
+      {
+        ownerId: 2,
+        address: " 5678 Domingo Drive",
+        city: "San Diego",
+        state: "CA",
+        country: "USA",
+        lat: 200.800,
+        lng: 600.400,
+        name: "Modern Beachfront Home",
+        description: "A fully furnished modern home by the beachfront. All amenities provided. Hot tub and private pool access included. Minimum 2 night booking. No cancellations allowed. No smoking or pets allowed",
+        price: 300.00
+      },
+      {
+        ownerId: 1,
         address: " 7890 Summer Lane",
         city: "Austin",
         state: "TX",
@@ -47,11 +46,11 @@ module.exports = {
         name: "Entire Home on the Apple Orchard Ranch",
         description: "Entire home available on the Apple Orchard Ranch. Basic amenities provided. Private pool access included. Minimum 1 night booking. Free cancellations allowed. Pets allowed.",
         price: 180.00
-    }
-  ])
+      }
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
--- backend/db/seeders/20220829231330-bookings-data.js
+++ backend/db/seeders/20220829231330-bookings-data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,15 +10,46 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+
+    return queryInterface.bulkInsert('Bookings', [
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20",
+
+
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20",
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate: "2021-08-16",
+        endDate: "2021-08-24",
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: "2021-12-16",
+        endDate: "2021-12-24",
+      }
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+
+    return queryInterface.bulkDelete('Bookings')
   }
 };
