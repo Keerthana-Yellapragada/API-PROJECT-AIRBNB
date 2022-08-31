00868fcf49a17d8fd1094d8146ab57ac82cb5df6
Keerthana Yellapragada <keerthanay@gmail.com>
2022-08-30 17:04:34 -0700

    changes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,17 +1,61 @@
 const express = require('express')
 const router = express.Router()
 const {
-    Spot
+    Spot,
+    Review,
+    SpotImage,
+    sequelize
 } = require('../../db/models');
 
 
-router.get('/', async (req,res) => {
 
-    const allSpots = await Spot.findAll({})
+router.get('/', async (req, res) => {
+    const allSpots = await Spot.findAll({
+        include: [{
+                model: Review,
+                attributes: []
+            },
+            {
+                model: SpotImage,
+                attributes: ['url']
+            }
+        ],
 
 
-    return res.json(allSpots)
+        attributes: {
+            include: [
+                [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]
+            ]
+        },
+        group: ['Spot.id'], // to return all spots
+        raw: true
+
+
+    })
+
+
+    return res.json({
+        allSpots
+    });
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
