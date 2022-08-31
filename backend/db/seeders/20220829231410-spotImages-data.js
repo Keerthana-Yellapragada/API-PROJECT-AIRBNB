8830782a6ff40b7da6553f6276968d375df78a45
Keerthana Yellapragada <keerthanay@gmail.com>
2022-08-31 10:50:25 -0700

    merge

--- backend/db/seeders/20220829231410-spotImages-data.js
+++ backend/db/seeders/20220829231410-spotImages-data.js
@@ -11,13 +11,11 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
-    return queryInterface.bulkInsert('SpotImages', [{},
+    return queryInterface.bulkInsert('SpotImages', [
       {
-
         spotId: 1,
-        url: "image url 1 ",
+        url: "image url",
         preview: true
-
       },
       {
         spotId: 2,
@@ -29,7 +27,11 @@ module.exports = {
         url: "image url",
         preview: true
       },
-
+      {
+        spotId: 3,
+        url: "image url",
+        preview: true
+      }
     ])
   },
 
