a7af0c9a6bab972662eb2ac2aef756f2757f729b
Keerthana Yellapragada <keerthanay@gmail.com>
2022-09-02 12:47:34 -0700

    recloned

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 
-// Create a variable called isProduction that will be true
+// Create a variable called isProduction that will be true.
 // if the environment is in production or not by checking the environment key in the configuration file(backend / config / index.js):
 
 const {
