1bd5988a89d2fc4227e7eb1326cb70f41c104933
Keerthana Yellapragada <keerthanay@gmail.com>
2022-09-17 10:49:46 -0700

    added login Modal Bonus phase

--- auth-me/frontend/src/components/Navigation/index.js
+++ auth-me/frontend/src/components/Navigation/index.js
@@ -1,7 +1,9 @@
+
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -11,12 +13,11 @@ function Navigation({ isLoaded }){
   if (sessionUser) {
     sessionLinks = (
       <ProfileButton user={sessionUser} />
-
     );
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
+        <LoginFormModal />
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
