611a78ac0dcfe4c3e8d5f0f16169ee01140acc17
Keerthana Yellapragada <keerthanay@gmail.com>
2022-09-19 10:17:28 -0700

    added CRUD thunks for spots

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -51,9 +51,10 @@ const updateSpot = (spot) => {
 
 
 //delete/remove a spot
-const removeSpot = (spot) => {
+const removeSpot = spotId => {
     return {
         type: REMOVE_SPOT
+
     }
 }
 
@@ -62,7 +63,58 @@ const removeSpot = (spot) => {
 
 // *****************************************************************************
 //************************************ THUNKS **********************************
-export const getAllSpots =
+
+// LOAD ALL SPOTS
+export const loadAllSpots = () => async dispatch => {
+    const response = await fetch(`/api/spots`);
+
+    if (response.ok) {
+        const spotsList = await response.json();
+        dispatch(getAllSpots(spotsList)) // dispatch using out action creator from above to get all spots
+    }
+}
+
+
+
+
+// GET A SPOT INFO
+export const getSpotInfo = spotId => async dispatch => {
+     const response = await fetch(`/api/spots/${spotId}`);
+
+     if (response.ok) {
+         const spotInfo = await response.json();
+         dispatch(getSpot(spotInfo)) // dispatch using out action creator from above to get spot's info by spotId
+     }
+}
+
+
+// EDIT A SPOT INFO
+export const editSpot = spotInfo => async dispatch => {
+    const response = await fetch(`/api/spots/${spotInfo.id}`, { //get the id from the spot obj and use that
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(spotInfo)
+    });
+
+    if (response.ok) {
+        const spot = await response.json();
+        dispatch(updateSpot(spot)) // dispatch using out action creator from above to get spot's info by spotId
+        return spot;
+    }
+}
+
+
+// DELETE A POKEMON
+export const deleteSpot = spotId => async dispatch => {
+    const response = await fetch(`/api/spots/${spotId}`, {
+         method: 'DELETE'
+    });
+
+    dispatch(removeSpot(spotId)) // dispatch the action create to remove a user
+    return response;
+}
 
 
 
