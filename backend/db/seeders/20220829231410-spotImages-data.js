8bf3a810a2c827d7af51f5494946b5c102815a59
Keerthana Yellapragada <keerthanay@gmail.com>
2022-10-11 10:47:49 -0700

    added new seed data

--- backend/db/seeders/20220829231410-spotImages-data.js
+++ backend/db/seeders/20220829231410-spotImages-data.js
@@ -26,6 +26,41 @@ module.exports = {
         spotId: 3,
         url: "https://images.unsplash.com/photo-1483190656465-2c49e54d29f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
         preview: true
+      },
+      {
+        spotId:4,
+        url: "https://images.unsplash.com/photo-1500314170724-9eca92963c76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        preview:true
+      },
+      {
+        spotId: 5,
+        url: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: "https://images.unsplash.com/photo-1524061511843-fd43443e3cb2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        preview: true
+      },
+      {
+        spotId:7,
+        url: "https://images.unsplash.com/photo-1525883360360-29a535e8cb17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        preview:true
+      },
+      {
+        spotId: 8,
+        url: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80",
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        preview: true
+      },
+      {
+        spotId: 10,
+        url: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlYWNoJTIwaG9tZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        preview: true
       }
     ])
   },
