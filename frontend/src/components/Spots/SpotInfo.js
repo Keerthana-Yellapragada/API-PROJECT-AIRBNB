7a7bb6049a9ac2cd939ee2dca92ee870dbdbe2d9
Keerthana Yellapragada <keerthanay@gmail.com>
2022-10-13 11:19:43 -0700

    Styled spotinfo page added icons

--- frontend/src/components/Spots/SpotInfo.js
+++ frontend/src/components/Spots/SpotInfo.js
@@ -17,13 +17,13 @@ const SpotInfo = () => {
     const dispatch = useDispatch();
     let { spotId } = useParams();
     spotId = parseInt(spotId)
-            console.log("SPOT ID IS ", spotId)
+    console.log("SPOT ID IS ", spotId)
 
-  useEffect(() => {
-      console.log("spotid useeffectis working")
-      dispatch(loadAllSpots());
-      dispatch(loadAllReviews(spotId))
-  }, [dispatch, spotId])
+    useEffect(() => {
+        console.log("spotid useeffectis working")
+        dispatch(loadAllSpots());
+        dispatch(loadAllReviews(spotId))
+    }, [dispatch, spotId])
 
     const allSpotsArray = useSelector(state => Object.values(state.spots))
     console.log("THIS IS ALLSPOTSARRAY ", allSpotsArray)
@@ -31,15 +31,15 @@ const SpotInfo = () => {
     const currentSpot = allSpotsArray.find(spot => spot.id == spotId)
     console.log("THIS IS CURRENT SPOT", currentSpot)
 
-    const allReviewsArray = useSelector(state=> Object.values(state.reviews))
-        console.log("THIS IS ALLREVIEWSARRAY ", allReviewsArray)
+    const allReviewsArray = useSelector(state => Object.values(state.reviews))
+    console.log("THIS IS ALLREVIEWSARRAY ", allReviewsArray)
 
     const sessionUser = useSelector(state => state.session.user)
     let userId;
     if (sessionUser) {
         userId = sessionUser.id
     }
-        console.log("userid", userId)
+    console.log("userid", userId)
 
     if (!currentSpot) {
         return null
@@ -47,24 +47,24 @@ const SpotInfo = () => {
 
 
 
-return (
-    <>
-        <div className='spot-info-page-container'>
-            <div className="spot-header-container">
+    return (
+        <>
+            <div className='spot-info-page-container'>
+                <div className="spot-header-container">
 
-                <div className="spot-header-name-container">
-                    <h1>{currentSpot.name}</h1>
-                </div>
+                    <div className="spot-header-name-container">
+                        <h1>{currentSpot.name}</h1>
+                    </div>
 
-                <div>
-                    < i className = "fa-solid fa-star" > </i>
-                    {
-                        `${currentSpot.avgRating} · ${allReviewsArray.length} reviews · ${currentSpot.city}, ${currentSpot.state}, ${currentSpot.country}`
-                    }
+                    <div>
+                        < i className="fa-solid fa-star" > </i>
+                        {
+                            `${currentSpot.avgRating} · ${allReviewsArray.length} reviews · ${currentSpot.city}, ${currentSpot.state}, ${currentSpot.country}`
+                        }
 
-                </div>
+                    </div>
 
-                {/* <div className='spot-header-info-container'>
+                    {/* <div className='spot-header-info-container'>
                     <div className="spot-stars">
                         <i className="fa-solid fa-star"></i>
                     </div>
@@ -80,58 +80,97 @@ return (
                 </div> */}
 
 
-            </div>
-
-            <div className='spot-image-container'>
-                <img src={currentSpot.previewImage} alt="preview-image" />
-            </div>
-
-
-            <div className='spot-details-container'>
-                <div className="hosted-by-container">
-                    {`Entire place hosted by ${currentSpot.ownerId}`}
-
-                </div>
-                <div className="spot-bed-bath-container">
-                      {" 4 guests · 2 bedrooms · 2 bed · 2 bath"}
-                </div>
-                <div className="spot-description-container">
-                    {currentSpot.description}
                 </div>
 
-                <div className='price-rating-side-container'>
-                    <div className="spot-description-price">
-                        {`$${currentSpot.price} night`}
+                <div className='spot-image-container'>
+                    <img src={currentSpot.previewImage} alt="preview-image" />
+                </div>
+
+
+                <div className='spot-details-container'>
+                    <div className="hosted-by-container">
+                        {`Entire place hosted by ${currentSpot.ownerId}`}
+                        <div className="owner-pic"><i className="fa-solid fa-circle-user"></i></div>
                     </div>
 
-                    <div className="spot-description-rating">
-                        <i class="fa-solid fa-star"></i>
-                        {currentSpot.avgRating}
+
+
+                    <div className="spot-bed-bath-container">
+                        {" 4 guests · 2 bedrooms · 2 bed · 2 bath"}
                     </div>
+
+                    <div className="aircover-title-image">
+                        < img src = "https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" />
+                    </div>
+
+                    <div className='aircover-info'>
+                        {
+                            "Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in."
+                        }
+                    </div>
+
+
+
+
+                    <div className="location-container">
+                        <div className='location-pin-image'><i class="fa-solid fa-location-dot"></i></div>
+                        <div className='location-info'>
+                            <div className='location-info-title'>
+                                {"Great Location"}
+                            </div>
+                            <div className='location-info-description'>
+                                {"95% of recent guests gave the location a 5-star rating"}
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                    <div className="spot-description-container">
+                        {currentSpot.description}
+                    </div>
+
+
+
+
+
+
+                    <div className='price-rating-side-container'>
+                        <div className="spot-description-price">
+                            {`$${currentSpot.price} night`}
+                        </div>
+
+                        <div className="spot-description-rating">
+                            <i class="fa-solid fa-star"></i>
+                            {currentSpot.avgRating}
+                        </div>
+                    </div>
+
                 </div>
 
-            </div>
 
+                <ReviewsBrowser />
+                <CreateReviewFormModal />
 
-            <ReviewsBrowser />
-            <CreateReviewFormModal />
-
-            <div className='edit-delete-spot-buttons-container'>
-{/*
+                <div className='edit-delete-spot-buttons-container'>
+                    {/*
                 {
                     userId && userId !== currentSpot.ownerId ? <CreateReviewFormModal /> : null
                 } */}
-                {
-                    userId && userId === currentSpot.ownerId ? < EditSpotFormModal /> : null
-                }
-                {
-                    userId && userId === currentSpot.ownerId ? < DeleteSpotFormModal /> : null
-                }
+                    {
+                        userId && userId === currentSpot.ownerId ? < EditSpotFormModal /> : null
+                    }
+                    {
+                        userId && userId === currentSpot.ownerId ? < DeleteSpotFormModal /> : null
+                    }
 
+                </div>
             </div>
-        </div>
-    </>
-)
+        </>
+    )
 }
 
 
