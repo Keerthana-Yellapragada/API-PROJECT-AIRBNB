0fe2145ddfc19b818c58a9959607499bf0060a6b
Keerthana Yellapragada <keerthanay@gmail.com>
2022-10-14 10:26:04 -0700

    removed entire html global styling

--- frontend/src/components/CreateReviewForm/index.js
+++ frontend/src/components/CreateReviewForm/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink, Route, useHistory, useParams } from 'react-router-dom';
 import createReview, { createNewReview, loadAllReviews } from "../../store/reviews"
-import "./CreateReview.css"
+
 
 const CreateReviewForm = ({closeProp}) => {
 
