d772253c75744eec1290288a5de343540ee6b3b5
Keerthana Yellapragada <keerthanay@gmail.com>
2022-10-19 10:59:47 -0700

    changes to reviewinfo

--- frontend/src/components/Reviews/ReviewInfo.js
+++ frontend/src/components/Reviews/ReviewInfo.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { loadUserReviews } from '../../store/reviews';
 import DeleteReviewForm from '../DeleteReview';
 import './ReviewInfo.css'
+import { loadAllSpots } from '../../store/spots';
 
 const ReviewInfo = () => {
     const dispatch = useDispatch();
@@ -12,13 +13,20 @@ const ReviewInfo = () => {
 
     useEffect(() => {
         dispatch(loadUserReviews())
+        // dispatch(loadAllSpots())
     }, [dispatch])
 
 
     const allReviewsArray = useSelector(state => Object.values(state.reviews))
     const currentReview = allReviewsArray.find(review => review.id === reviewId)
+    // console.log("current review", currentReview)
+    // console.log("current review spot : ", allReviewsArray[reviewId].Spot.name)
 
-    //console.log("current review is ", currentReview)
+    //get the spot that this review is for
+    // const  allSpots = useSelector(state=>Object.values(state.spots))
+    // let reviewSpot = allSpots.find(spot => spot.id === currentReview.spotId)
+    // console.log("REVIEW SPOT", reviewSpot)
+
 
     if (!currentReview) {
         return (<h1>Sorry, no review was found.</h1>)
@@ -29,9 +37,11 @@ const ReviewInfo = () => {
         <>
          <div className='current-review-card-flex-container'>
             <div className='current-review-card'>
+
                 <div className="current-review-title">
                     <i className="current-review-page-star fa-solid fa-star"></i>
                     <div className='current-review-rating'>{currentReview.stars}</div>
+                      {/* <div className='review-spot-name'>{currentReview.Spot.name}</div> */}
                 </div>
                 {/* <div  className="current-review-images-container">
                       {currentReview.ReviewImages.map(reviewImage => <img src={reviewImage.url}></img>)}
